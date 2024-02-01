bc84bf1f98515806a01dbe19077261793c52b40f
Elgraabi <silvatdb8@gmail.com>
2024-02-01 11:25:19 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,14 @@
+package boardgame;
+
+public class BoardException extends RuntimeException {
+
+	// Atributos 
+	private static final long serialVersionUID = 1L;
+	
+	// Metodos Especificos
+	public BoardException(String msg) {
+		super(msg);
+	}
+	
+	// Metodos Especiais
+}
